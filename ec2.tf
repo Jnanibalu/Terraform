6cf39be0c67fff1e966c8973ecfaa1f866d45247
Jnanibalu <137752529+Jnanibalu@users.noreply.github.com>
2023-09-13 20:38:57 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,11 @@
 provider "aws" {
     region = "ap-south-1"
-    access_key = "AKIA2RRNI6BE7KUMFSNP"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
   
 }
 resource "aws_instance" "Myec2" {
-    ami="ami-05552d2dcf89c9b24"
+    ami=""
     instance_type = "t2.micro"
     tags = {
       Name="terraform-instance"
